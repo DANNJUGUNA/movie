23fc4f3121c3ab1fcc19ce02c48dafab1e8ac5aa
DANNJUGUNA <ndan30071@gmail.com>
2023-09-19 10:52:36 +0300

    created the fetch function for the movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 // c94a5596
+import { useEffect } from "react";
+const API_URL="http://www.omdbapi.com?apikey=c94a5596"
 
-const API_URL="http://www.omdpapi.com?apikey=c94a5596"
-function App() {
-  
+const App=()=> {
+  const searchMovie= async(title)=>{
+    const response=await fetch(`${API_URL}&s=${title}`);
+    const data=await response.json();
+    console.log(data);
+  }
+  useEffect(()=>{
+     searchMovie();
+  },[])
   return (
     <div className="App">
       

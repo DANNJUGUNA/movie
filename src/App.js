aacc02da051623ad22c87ed9b5307b0fb3004a23
DANNJUGUNA <ndan30071@gmail.com>
2023-09-20 10:41:36 +0300

    made a display for the movies

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import {BiSearchAlt2} from "react-icons/bi"
 const API_URL="http://www.omdbapi.com?apikey=c94a5596"
 
 const App=()=> {
+  const movie1={"Poster"
+    : "N/A",
+    // "https://m.media-amazon.com/images/M/MV5BMDNmNDUwZGEtYmMxZS00NzU5LWEyMGMtOTRhNjBkNWNjNjMwXkEyXkFqcGdeQXVyNzA5MzMzNjk@._V1_SX300.jpg",
+    "Title" :"A Love Undefined",
+    "Type": "movie",
+    "Year" : "2016",
+    "imdbID" :"tt4955578"}
   const searchMovie= async(title)=>{
     const response=await fetch(`${API_URL}&s=${title}`);
     const data=await response.json();
@@ -31,7 +38,14 @@ const App=()=> {
             
       </div>
       <div className="container">
-
+            <div className="movie">
+              <div>
+                <p>{movie1.Year}</p>
+              </div>
+              <div>
+                <img src={movie1.Poster !== 'N/A'?movie1.Poster:"https://via.placeholder.com/400"} alt={movie1.Title}/>
+              </div>
+            </div>
       </div>
     </div>
   );
